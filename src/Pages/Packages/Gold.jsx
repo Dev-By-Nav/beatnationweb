9707eab2447657232f92104e241f30fff351731a
NVSXNGH <111112848+Dev-By-Nav@users.noreply.github.com>
2024-02-14 21:48:12 +0000

    Update Gold.jsx

--- src/Pages/Packages/Gold.jsx
+++ src/Pages/Packages/Gold.jsx
@@ -18,7 +18,7 @@ function Gold() {
     <div className='bg-bn-navy h-full w-full overflow-hidden'>
       <Navbar />
 
-      <h1 className="text-xl font-bold mb-4 mt-0 text-bn-gold font-montserrat flex flex-col items-center justify-center p-8">Gold</h1>
+      <h1 className="text-xl font-bold mb-4 mt-0 text-bn-gold font-montserrat flex flex-col items-center justify-center p-8">Beat Nation Magestic</h1>
 
 
       <div className='flex justify-center'>
